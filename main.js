2794d7cd62dc1ae88ba15d92651893ee8559a5a9
Randall Kincaid-Shatto <r_shatto@yahoo.com>
2018-11-21 08:55:28 -0800

    Fixed?

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ $(updateView)
 
 
 
+
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
     $.getJSON(BASE_URL + "/rides/count/per_month", getData)
@@ -27,4 +28,44 @@ for (var i = 0; i <=11; ++i){
 for (var i = 0; i <=9; ++i){
   chartData.push(rideData[2018] [i] [i+1]);
 } 
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["1", "2", "3", "4", "6","7", "8", "9", "10", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
+        datasets: [{
+            label: 'Rides Per Month',
+            data: chartData,
+            backgroundColor: [
+                'rgba(255, 99, 0, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 255, 255, 0.2',
+                'rgba(0,0,200, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 }
