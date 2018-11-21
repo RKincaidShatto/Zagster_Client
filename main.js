a5e4a1fe28e8f3cf680385c7a015d828399250d5
Randall Kincaid-Shatto <r_shatto@yahoo.com>
2018-11-21 06:56:59 -0800

    Trying to get data in chart (has errors)

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
+var chartData = []
+var rideData = []
 $(updateView)
+
 
 function updateView() {
     $.getJSON(BASE_URL + "/rides/count", updateRideCount)
-    $.getJSON(BASE_URL + "/rides/count/per_month", printData)
+    $.getJSON(BASE_URL + "/rides/count/per_month", getData)
 }
 
 function updateRideCount(data) {
     numberOfRides = data.count
     $("h2#rideCount").html(numberOfRides)
 }
-function printData(data){
+function getData(data){
   console.log(data)
+  rideData = data
+  console.log(rideData);
 }
+
+for (var i = 0; i <=3; ++i){
+    chartData.push(rideData[2016] [i] [i+9]);
+}
+for (var i = 0; i <=11; ++i){
+    chartData.push(rideData[2017] [i] [i+1]);
+}
+for (var i = 0; i <=9; ++i){
+    chartData.push(rideData[2018] [i] [i+1]);
+}
+console.log(chartData);
+
+
 
 /** @license
  * DHTML Snowstorm! JavaScript-based snow for web pages
